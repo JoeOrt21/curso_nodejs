7826a900a127659ba5a329ead0bac4bb85f9b742
Joe Daniel Ortiz Morales <isc18350328@gmail.com>
2022-01-03 17:20:55 -0600

    Separamos rutas y controladores

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,22 +1,17 @@
 const { Router } = require("express");
+const{
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    UsuariosDelete,
+} = require("../controllers/usuarios");
 
 const router = Router();
 
 //localhost:3001/usuarios
-router.get('/', function(req, res){
-    res.json({"msg": 'Hola a todos desde GET'});
-});
-
-router.post('/', function(req, res){
-    res.status(201).json({"msg": 'Hola a todos desde POST'});
-});
-
-router.put('/', function(req, res){
-    res.status(400).json({"msg": 'Hola a todos desde PUT'});
-});
-
-router.delete('/', function(req, res){
-    res.status(500).json({"msg": 'Hola a todos DELETE'});
-});
+router.get("/", usuariosGet);
+router.post("/", usuariosPost);
+router.put("/:id", usuariosPut);
+router.delete("/", UsuariosDelete);
 
 module.exports = router;
