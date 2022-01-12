0c9f6de32d6502fe69bb2b77d773124cd8c699cb
Joe Daniel Ortiz Morales <isc18350328@gmail.com>
2022-01-11 22:43:04 -0600

    Agregado BcryptJS para el Hash de la contraseña

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,6 +1,7 @@
 const { request, response } = require("express");
 const pool = require("../db/conexion");
 const usuariosQueries = require("../models/usuarios");
+const bcryptjs = require("bcryptjs");
 
 const usuariosGet = async (req = request, res = response) => {
   let conn;
@@ -27,13 +28,16 @@ const usuariosPost = async (req = request, res = response) => {
   let conn;
 
   try {
+    const salt = bcryptjs.genSaltSync();
+    const passwordHash = bcryptjs.hashSync(password, salt);
+
     conn = await pool.getConnection();
 
     const usuarios = await conn.query(usuariosQueries.insertUsuario,[
       nombre, 
       email, 
-      password, 
-      status,
+      passwordHash, 
+      status,   
     ]);
 
     res.json({ usuarios });
